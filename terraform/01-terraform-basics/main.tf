e98428c110416cd036e4574eef7098e611d22c81
benzWRC <worachai3@live.com>
2023-07-22 03:37:33 +0700

    Play with output

--- terraform/01-terraform-basics/main.tf
+++ terraform/01-terraform-basics/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "~> 3.0"
+        # version = "~> 5.9.0"
     }
   }
 }
@@ -10,4 +10,25 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
     region = "us-east-1"
+}
+
+# Plan - execute
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "my-s3-bucket-in28minutes-worachai-002"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+output "my_s3_bucket_versioning" {
+  value = aws_s3_bucket_versioning.versioning_example.versioning_configuration[0].status
+}
+
+output "my_s3_bucket_complete_details" {
+  value = aws_s3_bucket_versioning.versioning_example
 }
