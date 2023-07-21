da1ad740ce3306c6892e6b31d4585e0785368d68
benzWRC <worachai3@live.com>
2023-07-22 03:45:15 +0700

    Create iam user

--- terraform/01-terraform-basics/main.tf
+++ terraform/01-terraform-basics/main.tf
@@ -25,10 +25,19 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
   }
 }
 
+resource "aws_iam_user" "my_iam_user" {
+  name = "my_iam_user_abc"
+  
+}
+
 output "my_s3_bucket_versioning" {
   value = aws_s3_bucket_versioning.versioning_example.versioning_configuration[0].status
 }
 
 output "my_s3_bucket_complete_details" {
   value = aws_s3_bucket_versioning.versioning_example
+}
+
+output "my_iam_user_complete_details" {
+  value = aws_iam_user.my_iam_user
 }
