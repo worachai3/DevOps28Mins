c9e2cdbb957c19d6ab6847d737d58a7fe0d2b2da
benzWRC <worachai3@live.com>
2023-08-19 23:35:19 +0700

    Provision http server

--- terraform/05-ec2-instances/main.tf
+++ terraform/05-ec2-instances/main.tf
@@ -1,3 +1,7 @@
+variable "aws_key_pair" {
+  default = "./default-ec2.pem"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -30,7 +34,31 @@ resource "aws_security_group" "http_server_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-    tags = {
-        Name = "http_server_sg"
-    }
+  tags = {
+    Name = "http_server_sg"
+  }
 }
+
+resource "aws_instance" "http_server" {
+  ami                    = "ami-05548f9cecf47b442"
+  key_name               = "default-ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  subnet_id              = "subnet-0bd6c677b7d826064"
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",                                                                                //install httpd
+      "sudo service httpd start",                                                                                 //start httpd
+      "echo Welcome to in28minutes - Virtual Server is at ${self.public_dns} | sudo tee /var/www/html/index.html" //copy a file
+    ]
+  }
+}
+
